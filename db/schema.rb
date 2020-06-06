1764fb566ec631a2e93bf9ee4722f4dbbca26da9
Gustavo Mendes Maciel <gmmaciel.bh2@gmail.com>
2020-06-06 16:40:28 -0300

    Adicionada autenticação com devise

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_05_233026) do
+ActiveRecord::Schema.define(version: 2020_06_06_183913) do
 
   create_table "alimentos", force: :cascade do |t|
     t.string "nome"
@@ -33,6 +33,19 @@ ActiveRecord::Schema.define(version: 2020_06_05_233026) do
     t.float "sodio"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.datetime "confirmation_sent_at"
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
 end
