e4b799e315c5e983a2f40bc021f9d331be5b1fb9
Gustavo Mendes Maciel <gmmaciel.bh2@gmail.com>
2020-06-09 00:45:12 -0300

    Adicionado controlador Diario

---
+++ app/helpers/diario_helper.rb
@@ -0,0 +1,2 @@
+module DiarioHelper
+end
---
+++ test/controllers/diario_controller_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class DiarioControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+end
