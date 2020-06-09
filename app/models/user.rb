c85e7fec608e445e5cd6a143effa4673ab22b260
Gustavo Mendes Maciel <gmmaciel.bh2@gmail.com>
2020-06-09 00:21:18 -0300

    Adicionado before_destroy

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,16 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  before_destroy :destroy_alimentos
+
   has_many :alimentos, dependent: :destroy
   has_many :dias, dependent: :destroy
+
+  def destroy_alimentos
+    alimentos.each do |alimento|
+      # verifica se alimento está em alguma refeição
+      # se estiver então não destrói
+      # se não destrói
+    end
+  end
 end
