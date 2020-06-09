6b429f34e38d2bb8b96dd6029fd62894b61fb902
Gustavo Mendes Maciel <gmmaciel.bh2@gmail.com>
2020-06-09 01:09:13 -0300

    Adicionada função para formatar as datas

--- app/controllers/diario_controller.rb
+++ app/controllers/diario_controller.rb
@@ -1,4 +1,17 @@
 class DiarioController < ApplicationController
   def index
+    @user = User.find(params[:user_id])
+  end
+
+  def self.data_string(data)
+    if data.today?
+      return 'Hoje'
+    elsif data == Date.yesterday
+      return 'Ontem'
+    elsif data == Date.tomorrow
+      return 'Amanhã'
+    else
+      return I18n.l(data, format: '%A, %d de %B')
+    end
   end
 end
