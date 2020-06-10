71daa876e482bdcdd894f59735c7c85be1d801ee
Gustavo Mendes Maciel <gmmaciel.bh2@gmail.com>
2020-06-10 00:32:39 -0300

    Voltando atrás no controlador e view de diário

--- app/controllers/diario_controller.rb
+++ app/controllers/diario_controller.rb
@@ -1,35 +1,5 @@
 class DiarioController < ApplicationController
-
-  @@current_date = Date.today
-
   def index
-    @user = User.find(params[:user_id])
-
-    if params[:mode] == 'next'
-      @@current_date += 1
-    elsif params[:mode] == 'prev'
-      @@current_date -= 1
-    else
-      @@current_date = Date.today
-    end
-
-    lista_refeicoes()
+    session[:data_selecionada] = Date.today
   end
-
-  def self.data_string()
-    if @@current_date.today?
-      return 'Hoje'
-    elsif @@current_date == Date.yesterday
-      return 'Ontem'
-    elsif @@current_date == Date.tomorrow
-      return 'Amanhã'
-    else
-      return I18n.l(@@current_date, format: '%A, %d de %B')
-    end
-  end
-
-  private
-    def lista_refeicoes
-      # Lista todas as refeições de @user no dia @current_date
-    end
 end
